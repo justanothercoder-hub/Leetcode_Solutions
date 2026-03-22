7dfdbd299adcbab4c4d4ff956510cc43716bf9a1
just-another-coder <samikshanbhunia@gmail.com>
2026-03-22 15:46:39 +0530

    Implement function to find the middle of a linked list

---
+++ Middle of Linked List.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode sp=head;
+        ListNode fp=head;
+
+        while(fp.next != null && fp.next.next != null){
+            if(fp.next.next==null){sp = sp.next;}
+            else{sp=sp.next; fp=fp.next.next;}
+        }
+
+        if(fp.next==null) return sp;
+        else return sp.next;
+        
+    }
+}
