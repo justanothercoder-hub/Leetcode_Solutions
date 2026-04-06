f1912ddcdfa13cc9dae66cb2626e73a6ed32dcbe
just-another-coder <samikshanbhunia@gmail.com>
2026-04-06 21:45:02 +0530

    283. Move Zeroes
    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
    
    Note that you must do this in-place without making a copy of the array.
    
     
    
    Example 1:
    
    Input: nums = [0,1,0,3,12]
    Output: [1,3,12,0,0]
    Example 2:
    
    Input: nums = [0]
    Output: [0]

---
+++ MoveZeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0; int right =0;
+
+        for(right=0; right<nums.length; right++){
+            if(nums[right]!=0){
+                nums[left]=nums[right];
+                left++;
+            }
+        }
+
+        for(int i = left; i<nums.length; i++){
+            nums[i]=0;
+        }
+        
+    }
+}
